041e99be924098f7cf502105bad6c4bc7cc8e8dc
Gr-Stanislav <grstas1990@gmail.com>
2021-03-06 15:07:34 +0200

    lesson 1

---
+++ JS/script.js
@@ -0,0 +1,11 @@
+const btn = document.querySelector('.btn');
+const fon = document.querySelector('.black_fon');
+
+btn.onclick = function (){
+    fon.style.display = 'block';
+};
+
+const close = document.querySelector('.close');
+close.onclick = function (){
+    fon.style.display = 'none';
+};
